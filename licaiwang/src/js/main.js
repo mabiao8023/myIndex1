89837461a7c25c7d289fea85a8138ad6b0628d01
mabiao8023 <1184675699@qq.com>
2016-09-04 02:41:47 -0500

    requestWeb
    requestweb1.0

---
+++ licaiwang/src/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Administrator on 2016/7/30.
+ */
+
+$(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            items:1,
+            loop:true,
+            autoplay:true,
+            autoplayHoverPause:true,
+            autoplayTimeout:3000
+        }
+    )
+
+
+
+
+})
